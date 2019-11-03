c694a4647556e2cbbb325548e56959aed328cae8
Derek Wu <derekwu35@gmail.com>
2019-11-02 22:08:12 -0500

    recent

--- safetymesh/App.js
+++ safetymesh/App.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import { StatusBar, View } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import HomeScreen from "./src/screens/HomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
+import {StatusBar, View} from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import ChatScreen from './src/screens/ChatScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import globalStyles, {globalColors} from './src/Styles';
+
+const ChatStack = createStackNavigator(
+  {
+    ChatRouter: {
+      screen: ChatScreen,
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: globalColors.secondary,
+      },
+      headerTintColor: globalColors.white,
+      headerTitle: 'Chat',
+    },
+  },
+);
 
 const AppStack = createSwitchNavigator(
   {
-      TabStackRouter: {
-          screen: HomeScreen
-      },
-      LoginScreenRouter: {
-          screen: LoginScreen
-      }
+    TabStackRouter: {
+      screen: ChatStack,
+    },
+    LoginScreenRouter: {
+      screen: LoginScreen,
+    },
   },
   {
-      initialRouteName: 'LoginScreenRouter',
-  }
+    initialRouteName: 'LoginScreenRouter',
+  },
 );
 
 const AppContainer = createAppContainer(AppStack);
 
 export default class App extends React.Component {
-
   render() {
-      return (
-          <View>
-              <StatusBar barStyle="light-content"/>
-              <AppContainer/>
-          </View>
-      );
+    return (
+      <View style={globalStyles.globalContainer}>
+        <StatusBar barStyle="light-content" />
+        <AppContainer />
+      </View>
+    );
   }
 }
